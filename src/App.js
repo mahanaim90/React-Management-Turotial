2d17ca81146c17e6876ab6dc76fde2fb51884dbc
Steve <ims12266@gmail.com>
2021-02-22 17:10:16 -0500

    Create Cusomer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Lets develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'Steve',
+  'birthday':'hi lucas',
+  'gender':'popo',
+  'job':'hi daddy'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/2',
+    'name':'John',
+    'birthday':'hi lucas',
+    'gender':'popo',
+    'job':'hi daddy'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.com/64/64/3',
+    'name':'Lucas',
+    'birthday':'hi lucas',
+    'gender':'popo',
+    'job':'hi daddy'
+    }
+]
+class App extends Component{
+  render(){
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
